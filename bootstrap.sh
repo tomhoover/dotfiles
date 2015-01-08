8e69619e6d62eab10fab9e48e6047bb1c79cee00
Tom Hoover <tom@hisword.net>
2015-01-07 22:25:25 -0600

    added prelimary code to install homebrew during bootstrap (still needs testing)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+if [ `uname` = Darwin ] ; then
+	echo "verify Xcode and git are installed"
+	[ `which brew` ] || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+	brew update || exit
+	brew doctor || exit
+	brew upgrade || exit
+	brew install mr
+fi
+
 if [ `uname` = Linux ] ; then
 	sudo apt-get update && sudo apt-get install git mr
-	if [ ! -r ~/.mrconfig ] ; then
-		cd $HOME && mr -t -i bootstrap https://raw.githubusercontent.com/tomhoover/mr-castle/master/home/.mrconfig
-	fi
+fi
+
+if [ ! -r ~/.mrconfig ] ; then
+	cd $HOME && mr -t -i bootstrap https://raw.githubusercontent.com/tomhoover/mr-castle/master/home/.mrconfig
 fi
 
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
