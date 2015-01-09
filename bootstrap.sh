6d3c7e33a290320295f0985d16f0dde68ebf327a
Tom Hoover <tom@hisword.net>
2015-01-09 13:43:13 -0600

    convert from homeshick to vcsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,25 +6,27 @@ if [ `uname` = Darwin ] ; then
 	brew update || exit
 	brew doctor || exit
 	brew upgrade || exit
-	brew install mr
+	brew install mr vcsh
 fi
 
 if [ `uname` = Linux ] ; then
-	sudo apt-get update && sudo apt-get install git mr
+	sudo apt-get update && sudo apt-get install git mr vcsh
 fi
 
 if [ ! -r ~/.mrconfig ] ; then
-	cd $HOME && mr -t -i bootstrap https://raw.githubusercontent.com/tomhoover/mr-castle/master/home/.mrconfig
+	# cd $HOME && mr -t -i bootstrap https://raw.githubusercontent.com/tomhoover/mr-castle/master/home/.mrconfig
+	cd $HOME && vcsh clone git@github.com:tomhoover/vcsh_mr.git mr
+	mr up
 fi
 
-source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-homeshick link
+#source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+#homeshick link
 
 #add the following to crontab:
 # @weekly comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) | grep -v ^linux-headers- > ~/.config/apt-mark/installed
 if [ `uname` = Linux ] ; then
 	sudo aptitude install $(cat ~/.config/apt-mark/installed)
-	sudo apt-mark manual $(cat ~/.config/apt-mark/installed) 	# this line is probably not needed, but added for good measure
+	# sudo apt-mark manual $(cat ~/.config/apt-mark/installed) 	# this line is probably not needed, but added for good measure
 fi
 
 echo ""
