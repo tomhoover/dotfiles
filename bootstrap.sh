d69a905cfa0b25d78b6aa6106fe200a5d761be0e
Tom Hoover <tom@hisword.net>
2016-11-12 16:21:41 -0600

    modify to bootstrap my 'unraid' media server

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,21 +11,38 @@ if [ `uname` = Darwin ] ; then
     brew install mr vcsh
 fi
 
-if [ `uname` = Linux ] ; then
+if [ `hostname -s` = unraid ] ; then
+    cd /usr/bin && ln -sf 'vim' 'vi'
+    mkdir -p ~/src
+    cd ~/src
+    if [ ! -d "vcsh/.git" ] ; then
+        git clone https://github.com/RichiH/vcsh.git
+        cd /usr/local/bin && ln -s ~/src/vcsh/vcsh
+    fi
+    cd ~/src
+    if [ ! -d "myrepos/.git" ] ; then
+        git clone https://github.com/joeyh/myrepos.git
+        cd /usr/local/bin && ln -s ~/src/myrepos/mr
+    fi
+elif [ `uname` = Linux ] ; then
     sudo apt-get update && sudo apt-get install git mr vcsh
 fi
 
-curl http://j.mp/spf13-vim3 -L -o - | sh
+if [ ! -d "$HOME/.spf13-vim-3" ] ; then
+    cd && curl http://j.mp/spf13-vim3 -L -o - | sh
+    mv ~/.vimrc.local ~/.vimrc.local.bak
+else
+    cd ~/.spf13-vim-3
+    git pull
+    vim +BundleInstall! +BundleClean +q
+fi
 
 #if [ ! -r ~/.mrconfig ] ; then
     #cd $HOME && mr -t -i bootstrap https://raw.githubusercontent.com/tomhoover/mr-castle/master/home/.mrconfig
 #fi
 
-#source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-#homeshick link
-
-vcsh clone git@github.com:tomhoover/mr-vcsh.git mr
-mr up
+/usr/local/bin/vcsh clone git@github.com:tomhoover/mr-vcsh.git mr
+/usr/local/bin/mr up
 
 #add the following to crontab:
 # @weekly brew list > ~/.config/homebrew/brew.installed
@@ -39,7 +56,7 @@ fi
 #add the following to crontab:
 # @weekly comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) | grep -v ^linux-headers- > ~/.config/apt/installed
 
-if [ `uname` = Linux ] ; then
+if [ ! `hostname -s` = unraid ] && [ `uname` = Linux ] ; then
     sudo aptitude install $(cat ~/.config/apt/installed)
     # sudo apt-mark manual $(cat ~/.config/apt/installed) 	# this line is probably not needed, but added for good measure
 fi
