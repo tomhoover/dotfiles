1d56b891b85eba336ce537ec8a024ba7a4a0d225
Tom Hoover <tom@hisword.net>
2023-02-17 11:51:53 -0600

    Add pipx/install.sh script

---
+++ pipx/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#pip install pipx
+
+pipx install black
+pipx install flake8
+pipx install ipython
+pipx install mypy
+pipx install pipenv
+pipx install pre-commit
+pipx install pylint
+pipx install tmuxp
