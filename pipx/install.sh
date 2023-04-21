c4484eb452c005876126298d1ec6f9dc8d6bd54b
Tom Hoover <tom@hisword.net>
2023-04-21 14:13:33 -0500

    Do not install mypy with pipx

--- pipx/install.sh
+++ pipx/install.sh
@@ -5,7 +5,7 @@ pip list | grep pipx  >/dev/null 2>&1 || ( pip install pipx && pipx reinstall-al
 command -v black      >/dev/null 2>&1 || pipx install black
 command -v flake8     >/dev/null 2>&1 || pipx install flake8
 command -v ipython    >/dev/null 2>&1 || pipx install ipython
-command -v mypy       >/dev/null 2>&1 || pipx install mypy
+# command -v mypy     >/dev/null 2>&1 || pipx install mypy
 # command -v pipenv   >/dev/null 2>&1 || pipx install pipenv
 command -v pre-commit >/dev/null 2>&1 || pipx install pre-commit
 command -v pylint     >/dev/null 2>&1 || pipx install pylint
