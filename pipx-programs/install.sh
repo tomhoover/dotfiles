4ccf078b9828811f44b6f82890e2157134bdd803
Tom Hoover <tom@hisword.net>
2023-05-08 10:21:56 -0500

    Split pipx-programs/install.sh from pipx/install.sh

---
+++ pipx-programs/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+command -v pipx       >/dev/null 2>&1 && pipx reinstall-all
+
+command -v black      >/dev/null 2>&1 || pipx install black
+command -v flake8     >/dev/null 2>&1 || pipx install flake8
+command -v ipython    >/dev/null 2>&1 || pipx install ipython
+# command -v mypy     >/dev/null 2>&1 || pipx install mypy
+# command -v pipenv   >/dev/null 2>&1 || pipx install pipenv
+command -v pre-commit >/dev/null 2>&1 || pipx install pre-commit
+command -v pylint     >/dev/null 2>&1 || pipx install pylint
+command -v tmuxp      >/dev/null 2>&1 || pipx install tmuxp
--- pipx/install.sh
+++ pipx/install.sh
@@ -8,17 +8,6 @@ if [[ $(pyenv global) == "system" ]]; then
     exit 1
 else
     eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)" && pyenv activate $(pyenv global | tail -1)
-
-    pip list | grep pipx  >/dev/null 2>&1 || ( pip install pipx && pipx reinstall-all )
-
-    command -v black      >/dev/null 2>&1 || pipx install black
-    command -v flake8     >/dev/null 2>&1 || pipx install flake8
-    command -v ipython    >/dev/null 2>&1 || pipx install ipython
-    # command -v mypy     >/dev/null 2>&1 || pipx install mypy
-    # command -v pipenv   >/dev/null 2>&1 || pipx install pipenv
-    command -v pre-commit >/dev/null 2>&1 || pipx install pre-commit
-    command -v pylint     >/dev/null 2>&1 || pipx install pylint
-    command -v tmuxp      >/dev/null 2>&1 || pipx install tmuxp
-
+    pip list | grep pipx >/dev/null 2>&1 || pip install pipx
     pyenv deactivate
 fi
