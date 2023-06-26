358efed16070f507d8159fd20e75d4cd29d7d602
Tom Hoover <tom@hisword.net>
2023-06-26 07:43:27 -0500

    Install ansible with pipx

--- pipx-programs/install.sh
+++ pipx-programs/install.sh
@@ -2,6 +2,14 @@
 
 command -v pipx       >/dev/null 2>&1 && pipx reinstall-all
 
+# https://github.com/pypa/pipx/issues/20
+command -v ansible    >/dev/null 2>&1 || pipx install --include-deps ansible
+# If you need [community modules and plugins which are now part of collections](https://github.com/ansible-collections/overview#now-ansible-2-10-and-later),
+# you can then inject them:
+    # pipx inject ansible $module
+    #   injected package $module into venv ansible
+    #   done! ✨ 🌟 ✨
+
 command -v black      >/dev/null 2>&1 || pipx install black
 command -v flake8     >/dev/null 2>&1 || pipx install flake8
 command -v ipython    >/dev/null 2>&1 || pipx install ipython
