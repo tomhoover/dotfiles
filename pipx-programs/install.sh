7a359151944e96193b703267fb3edd7af0168e15
Tom Hoover <tom@hisword.net>
2023-09-20 14:11:32 -0500

    Commented out line containing 'pipx reinstall-all' due to error message in arch
    See https://github.com/tomhoover/dotfiles/issues/4

--- pipx-programs/install.sh
+++ pipx-programs/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-command -v pipx       >/dev/null 2>&1 && pipx reinstall-all
+# TODO: commented out the following due to the following error on arch:
+# Can not find pip. You may encounter issues uninstalling packages. Remove /home/tom/.local/pipx/shared and run 'pipx reinstall-all' to fix them.
+# command -v pipx       >/dev/null 2>&1 && pipx reinstall-all
 
 # https://github.com/pypa/pipx/issues/20
 command -v ansible    >/dev/null 2>&1 || pipx install --include-deps ansible
@@ -8,9 +10,9 @@ command -v ansible    >/dev/null 2>&1 || pipx install --include-deps ansible
     # pipx inject ansible $module
     #   injected package $module into venv ansible
     #   done! ✨ 🌟 ✨
-jq -r '.injected_packages | keys' ~/.local/pipx/venvs/ansible/pipx_metadata.json | grep requests || pipx inject ansible requests
-jq -r '.injected_packages | keys' ~/.local/pipx/venvs/ansible/pipx_metadata.json | grep passlib  || pipx inject ansible passlib
-jq -r '.injected_packages | keys' ~/.local/pipx/venvs/ansible/pipx_metadata.json | grep netaddr  || pipx inject ansible netaddr
+jq -r '.injected_packages | keys' ~/.local/pipx/venvs/ansible/pipx_metadata.json | grep requests >/dev/null || pipx inject ansible requests
+jq -r '.injected_packages | keys' ~/.local/pipx/venvs/ansible/pipx_metadata.json | grep passlib  >/dev/null || pipx inject ansible passlib
+jq -r '.injected_packages | keys' ~/.local/pipx/venvs/ansible/pipx_metadata.json | grep netaddr  >/dev/null || pipx inject ansible netaddr
 
 command -v black      >/dev/null 2>&1 || pipx install black
 command -v flake8     >/dev/null 2>&1 || pipx install flake8
@@ -24,4 +26,4 @@ command -v tmuxp      >/dev/null 2>&1 || pipx install tmuxp
 #### pipx inject ansible molecule
 command -v molecule   >/dev/null 2>&1 || pipx install molecule
 #### pipx inject molecule molecule-docker
-jq -r '.injected_packages | keys' ~/.local/pipx/venvs/molecule/pipx_metadata.json | grep molecule-plugins || { pipx inject molecule molecule-plugins; pipx inject molecule 'molecule-plugins[docker]'; }
+jq -r '.injected_packages | keys' ~/.local/pipx/venvs/molecule/pipx_metadata.json | grep molecule-plugins >/dev/null || { pipx inject molecule molecule-plugins; pipx inject molecule 'molecule-plugins[docker]'; }
