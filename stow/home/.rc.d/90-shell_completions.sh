a0bcd164c5226e756f63a19b5fbe8180a3121a8d
Tom Hoover <tom@hisword.net>
2026-03-16 13:38:52 -0500

    add codex shell completion

--- stow/home/.rc.d/90-shell_completions.sh
+++ stow/home/.rc.d/90-shell_completions.sh
@@ -1,6 +1,7 @@
 # shellcheck shell=bash
 
+if command -v codex &>/dev/null; then eval "$(codex completion "${SHEL}")"; fi
 if command -v register-python-argcomplete &>/dev/null; then eval "$(register-python-argcomplete pipx)"; fi # enable pipx completion
-if command -v ruff                        &>/dev/null; then eval "$(ruff generate-shell-completion "${SHEL}")"; fi
-if command -v uv                          &>/dev/null; then eval "$(uv generate-shell-completion "${SHEL}")"; fi
-if command -v uvx                         &>/dev/null; then eval "$(uvx --generate-shell-completion "${SHEL}")"; fi
+if command -v ruff &>/dev/null; then eval "$(ruff generate-shell-completion "${SHEL}")"; fi
+if command -v uv &>/dev/null; then eval "$(uv generate-shell-completion "${SHEL}")"; fi
+if command -v uvx &>/dev/null; then eval "$(uvx --generate-shell-completion "${SHEL}")"; fi
