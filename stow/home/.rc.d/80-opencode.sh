d8e54377094b822e3a3b10edcbf7ae71478d36df
Tom Hoover <tom@hisword.net>
2026-03-28 16:54:47 -0500

    fix: opencode completion only supports zsh

--- stow/home/.rc.d/80-opencode.sh
+++ stow/home/.rc.d/80-opencode.sh
@@ -3,13 +3,15 @@
 if command -v "$HOME/.opencode/bin/opencode" &>/dev/null; then
   export PATH="$HOME/.opencode/bin:$PATH"
 
-  _bin="$HOME/.opencode/bin/opencode"
-  _cache="${XDG_CACHE_HOME:-$HOME/.cache}/opencode/completion.${SHEL}"
-  if [[ ! -f "$_cache" || "$_bin" -nt "$_cache" ]]; then
-    mkdir -p "${_cache%/*}"
-    "$_bin" completion >"$_cache"
+  if [ "$SHEL" = "zsh" ]; then
+    _bin="$HOME/.opencode/bin/opencode"
+    _cache="${XDG_CACHE_HOME:-$HOME/.cache}/opencode/completion.zsh"
+    if [[ ! -f "$_cache" || "$_bin" -nt "$_cache" ]]; then
+      mkdir -p "${_cache%/*}"
+      "$_bin" completion zsh >"$_cache"
+    fi
+    # shellcheck disable=SC1090
+    source "$_cache"
+    unset _bin _cache
   fi
-  # shellcheck disable=SC1090
-  source "$_cache"
-  unset _bin _cache
 fi
