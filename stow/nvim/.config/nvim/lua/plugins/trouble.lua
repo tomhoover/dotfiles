634d9a89294a3fa3288a4d71e5d9441430345737
Tom Hoover <tom@hisword.net>
2026-03-28 12:38:46 -0500

    chore: revise lazyvim's 'trouble' options

---
+++ stow/nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    use_diagnostic_signs = true,
+    auto_close = true,
+    modes = {
+      diagnostics = {
+        auto_open = true,
+      },
+    },
+  },
+}
