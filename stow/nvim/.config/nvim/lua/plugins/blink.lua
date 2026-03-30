63b8046f9762d37ef736ada2885f0b777216b9c6
Tom Hoover <tom@hisword.net>
2026-03-30 11:28:21 -0500

    chore: experimenting with copilot.lua settings

--- stow/nvim/.config/nvim/lua/plugins/blink.lua
+++ stow/nvim/.config/nvim/lua/plugins/blink.lua
@@ -2,5 +2,15 @@ return {
   {
     "zbirenbaum/copilot.lua",
     event = { "BufReadPost", "BufNewFile" },
+    -- requires = {
+    --   "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+    -- },
+    -- cmd = "Copilot",
+    -- event = "InsertEnter",
+    -- config = function()
+    --   require("copilot").setup({
+    --     -- copilot_model = "", -- default: gpt-41-copilot, enter :Copilot model list
+    --   })
+    -- end,
   },
 }
