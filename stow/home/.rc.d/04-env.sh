14ce75b4afa76461b9cb01f843153e5f228ce211
Tom Hoover <tom@hisword.net>
2026-04-21 13:24:17 -0500

    chore: change pager options

--- stow/home/.rc.d/04-env.sh
+++ stow/home/.rc.d/04-env.sh
@@ -23,6 +23,11 @@ else
   export EDITOR='vi'
 fi
 
+# export LESS='-eFgiMRSw -z-4'
+export LESS='-eFiRX'
+export PAGER='less'
+# less --tabs=4 -eiFRSX
+
 # Prefer US English and use UTF-8.
 export LANG='en_US.UTF-8'
 export LC_ALL='en_US.UTF-8'
